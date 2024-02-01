5455f7909839b1a6b812c72d9e8449250b14594f
PurnimaMahato <purnimamahato782@gmail.com>
2024-02-01 21:08:32 +0530

    updation

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 function goto() {
     window.location.href="Reg/index.html";
   }
+  const carouselContainer = document.querySelector('.carousel-container');
+
+  let slideIndex = 0;
+
+  function showSlides() {
+    const slides = document.querySelectorAll('.carousel-slide');
+    if (slideIndex >= slides.length) {
+      slideIndex = 0;
+    }
+
+    const translateValue = -slideIndex * 100;
+    carouselContainer.style.transform = `translateX(${translateValue}%)`;
+
+    slideIndex++;
+  }
+
+  // Set interval to change slides every 3 seconds (adjust as needed)
+  setInterval(showSlides, 4000);
